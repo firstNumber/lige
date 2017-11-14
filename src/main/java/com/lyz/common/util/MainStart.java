58c3bc77067caca9c3e8f0f63e5e8ba7f8174706
liyongzhen <liyongzhen@zhidianlife.com>
2017-11-14 10:46:37 +0800

    21321

--- src/main/java/com/lyz/common/util/MainStart.java
+++ src/main/java/com/lyz/common/util/MainStart.java
@@ -4,16 +4,20 @@ import com.lyz.common.util.teard.ThreadTest;
 
 public class MainStart {
 	public static void main(String[] args) throws InterruptedException {
-		ThreadTest test0 = new ThreadTest();
-		new Thread(test0,"线程1").start();
-		ThreadTest test = new ThreadTest();
-		test.orderNumber ="一一一一一一";
-		new Thread(test,"线程2").start();
+//		ThreadTest test0 = new ThreadTest();
+//		new Thread(test0,"线程1").start();
+//		ThreadTest test = new ThreadTest();
+//		test.orderNumber ="一一一一一一";
+//		new Thread(test,"线程2").start();
+//		
+//		ThreadTest test1 = new ThreadTest();
+//		test1.orderNumber ="二二二二二二";
+//		new Thread(test1,"线程3").start();
+//		
+//		System.out.println("hello word featuril002");
 		
-		ThreadTest test1 = new ThreadTest();
-		test1.orderNumber ="二二二二二二";
-		new Thread(test1,"线程3").start();
+		System.out.println((String)null);
 		
-		System.out.println("hello word featuril002");
+		
 	}
 }
