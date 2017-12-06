ebc701955c19d3106e5d8cb4a5df8a488dc35d7f
lyz <15910995356@163.com>
2017-12-06 23:35:17 +0800

    add

--- src/main/java/com/lyz/user/dao/CarboxDao.java
+++ src/main/java/com/lyz/user/dao/CarboxDao.java
@@ -1,9 +1,12 @@
 package com.lyz.user.dao;
 
 import com.lyz.common.dao.BaseDao;
+import com.lyz.common.dynamic.DataSource;
 import com.lyz.user.model.Carbox;
 
-public interface CarboxDao extends BaseDao<Carbox>{
 
+
+public interface CarboxDao extends BaseDao<Carbox>{
+	 public Carbox  querycarbox();
 }
 
